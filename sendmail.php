fc9af188421f18f6f72d458248638f9d8af4f34a
Projeto Livro Digital <metonimiacontato@gmail.com>
2021-10-07 15:35:15 -0300

    -> Formulário de Contato PHP

--- sendmail.php
+++ sendmail.php
@@ -1,22 +1,22 @@
 <?php
     //Email: metonimiacontato@gmail.com
     
-    if(isset($_POST('[email']) && !empty($_POST(['email'])){
+    if(isset($_POST'[email']) && !empty($_POST['email'])){
 
-        $nome    =   addlashers($_POST(['name']))
-        $email    =   addlashers($_POST(['email']))
-        $assunto  =   addlashers($_POST(['subject']))
-        $mensagem  =   addlashers($_POST(['message']))
-        
+        $nome    =   addlashers($_POST['name'])
+        $email    =   addlashers($_POST['email'])
+        $assunto  =   addlashers($_POST['subject'])
+        $mensagem  =   addlashers($_POST['message'])
+    
 
         $to = "metonimiacontato@gmail.com"
         $subject = "* Suporte Metonímia *"
-        $body = "Nome: ".$nome. "\r\n"
-                "Email: ".$email. "\r\n"
+        $body = "Nome: ".$nome. "\r\n".
+                "Email: ".$email. "\r\n".
                 "Mensagem: ".$mensagem;
-        $header = "From:luizlimacontato1171@gmail.com"."\r\n"
-                    ."Reply-To:".$email."\r\n"
-                    ."X=Mailer:PHP/".phpversion();
+        $header = "From:luizlimacontato1171@gmail.com"."\r\n".
+                    "Reply-To:".$email."\r\n".
+                    "X=Mailer:PHP/".phpversion();
 
 
        if(mail($to, $subject, $message, $body, $header)){
